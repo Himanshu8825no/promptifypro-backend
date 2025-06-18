28bb93546dc447d93709ece949ee7234844018cd
Himanshu kumar <himanshukumar94321@gmail.com>
2025-06-18 22:16:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,6 @@ def home():
     return "Promptify Pro Backend is Running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
